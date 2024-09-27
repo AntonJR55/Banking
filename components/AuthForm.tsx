8a36ec3ff3c6f291467ca296d21af542fd639549
Andrey <komaras@belpost.by>
2024-09-27 15:51:28 +0300

    added sign in

---
+++ components/AuthForm.tsx
@@ -0,0 +1,116 @@
+"use client";
+
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import Link from "next/link";
+import Image from "next/image";
+import { z } from "zod";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { Form } from "@/components/ui/form";
+import { Button } from "@/components/ui/button";
+import { authFormSchema } from "@/lib/utils";
+
+import CustomInput from "./CustomInput";
+import { Loader2 } from "lucide-react";
+
+const AuthForm = ({ type }: { type: string }) => {
+    const [user, setUser] = useState(null);
+    const [isLoading, setIsLoading] = useState(false);
+    const form = useForm<z.infer<typeof authFormSchema>>({
+        resolver: zodResolver(authFormSchema),
+        defaultValues: {
+            email: "",
+            password: "",
+        },
+    });
+
+    function onSubmit(values: z.infer<typeof authFormSchema>) {
+        // Do something with the form values.
+        // ✅ This will be type-safe and validated.
+        setIsLoading(true);
+        console.log(values);
+        setIsLoading(false);
+    }
+
+    return (
+        <div className="auth-form">
+            <header className="flex flex-col gap-5 md:gap-8">
+                <Link
+                    href="/"
+                    className="cursor-pointer flex items-center gap-1"
+                >
+                    <Image
+                        src="/icons/logo.svg"
+                        width={34}
+                        height={34}
+                        alt="Horizon logo"
+                    />
+                    <h1 className="text-26 font-ibm-plex-serif font-bold text-black-1">
+                        Horizon
+                    </h1>
+                </Link>
+
+                <div className="flex flex-col gap-1 md:gap-3">
+                    <h1 className="text-24 lg-text-36 font-semibold text-gray-900">
+                        {user
+                            ? "Link Account"
+                            : type === "sign-in"
+                            ? "Sign In"
+                            : "Sign Up"}
+                        <p className="text-16 font-normal text-gray-600">
+                            {user
+                                ? "Link your account to get started"
+                                : "Please enter your details"}
+                        </p>
+                    </h1>
+                </div>
+            </header>
+            {user ? (
+                <div className="flex flex-col gap-4"></div>
+            ) : (
+                <>
+                    <Form {...form}>
+                        <form
+                            onSubmit={form.handleSubmit(onSubmit)}
+                            className="space-y-8"
+                        >
+                            <CustomInput
+                                control={form.control}
+                                name="email"
+                                label="Email"
+                                placeholder="Enter your email"
+                            />
+                            <CustomInput
+                                control={form.control}
+                                name="password"
+                                label="Password"
+                                placeholder="Enter your password"
+                            />
+                            <Button
+                                type="submit"
+                                disabled={isLoading}
+                                className="form-btn"
+                            >
+                                {isLoading ? (
+                                    <>
+                                        <Loader2
+                                            size={20}
+                                            className="animate-spin"
+                                        />
+                                        &nbsp;Loading...
+                                    </>
+                                ) : type === "sign-in" ? (
+                                    "Sign In"
+                                ) : (
+                                    "Sign Up"
+                                )}
+                            </Button>
+                        </form>
+                    </Form>
+                </>
+            )}
+        </div>
+    );
+};
+
+export default AuthForm;
